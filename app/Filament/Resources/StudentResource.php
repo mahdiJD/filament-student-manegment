58dff3e05c1053a3c9b3e78e4c939c5b0d1739af
Mahdi <bijarimahdi@gmail.com>
2024-11-25 20:29:34 +0330

    searche filter

--- app/Filament/Resources/StudentResource.php
+++ app/Filament/Resources/StudentResource.php
@@ -2,8 +2,10 @@
 
 namespace App\Filament\Resources;
 
+use App\Exports\StudentsExport;
 use App\Filament\Resources\StudentResource\Pages;
 use App\Filament\Resources\StudentResource\RelationManagers;
+use App\Models\Classes;
 use App\Models\Section;
 use App\Models\Student;
 use Filament\Forms;
@@ -13,10 +15,15 @@ use Filament\Forms\Form;
 use Filament\Forms\Get;
 use Filament\Resources\Resource;
 use Filament\Tables;
+use Filament\Tables\Actions\BulkAction;
 use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Filters\Filter;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Livewire\Mechanisms\HandleComponents\Synthesizers\CollectionSynth;
+use Maatwebsite\Excel\Facades\Excel;
 
 class StudentResource extends Resource
 {
@@ -47,7 +54,7 @@ class StudentResource extends Resource
 
     public static function table(Table $table): Table
     {
-        $table
+        return $table
             ->columns([
 
                 TextColumn::make('name')->
@@ -60,7 +67,24 @@ class StudentResource extends Resource
                 TextColumn::make('section.name')->badge(),
             ])
             ->filters([
-                //
+                Filter::make('class-section-filter')->form([
+                    Select::make('class_id')->
+                        label('Filter By Class')->placeholder('Select a Class')->options(Classes::pluck('name','id')->toArray()),
+                    Select::make('section_id')->
+                        label('Filter By Section')->placeholder('Select a Section')->options(function(Get $get){
+                            $classId = $get('class_id');
+                            if($classId){
+                                return Section::where('class_id',$classId)->pluck('name','id')->toArray();
+                            }
+                        })
+                ])
+                ->query(function (Builder $query, array $data):Builder{
+                    return $query->when($data['class_id'],function($query) use($data){
+                        return $query->where('class_id', $data['class_id']);
+                    })->when($data['section_id'],function($query) use($data){
+                        return $query->where('section_id', $data['section_id']);
+                    });
+                }),
             ])
             ->actions([
                 Tables\Actions\EditAction::make(),
@@ -68,10 +92,12 @@ class StudentResource extends Resource
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
                     Tables\Actions\DeleteBulkAction::make(),
+                    BulkAction::make('export')->
+                    label('Export to exel')->icon('heroicon-o-document-arrow-down')->action(function(Collection $recording){
+                        return Excel::download(new StudentsExport($recording), 'student.xlsx');
+                    })
                 ]),
             ]);
-            // dd($table);
-            return $table;
     }
 
     public static function getRelations(): array
